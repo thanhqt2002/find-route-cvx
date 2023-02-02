d620c1cc0e40512b974d69d93965680329f0a10c
Tran Quang Thanh <thanhqt2002@gmail.com>
2023-02-02 18:19:50 +0700

    clean up data

--- solver.py
+++ solver.py
@@ -1,10 +1,21 @@
 from entity import pools, tokens, prices
 
-pools = pools.load_pools("./data/uni.json")
+pools = pools.load_pools("./data/uni.json", "./data/balancer-weighted.json")
 tokens = tokens.load_tokens("./data/tokens.json")
 prices = prices.load_prices("./data/prices.json")
 
-print(len(tokens), len(pools), len(prices))
+# Clean up data
 
-for price in prices:
-    print(price.get_preferred_price())
+token_by_address = {token.address: token for token in tokens}
+price_by_address = {price.address: price for price in prices}
+
+if token_by_address.keys() != price_by_address.keys():
+    raise Exception("token_address must exist in both price and token data")
+
+# filter out pools that contains a token that we don't have data
+pools = list(filter(lambda pool: all([(token.address in token_by_address) for token in pool.tokens]), pools))
+
+# n = number of tokens
+n = len(token_by_address)
+# m = number of pools
+m = len(pools)
